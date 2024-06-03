4fcc31917dd286bbcb733caba3a6a7b482688fea
anton_penkov <anton.penkov1@gmail.com>
2024-06-03 19:28:53 +0200

    Initial Commit

---
+++ AviaApp/AviaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AviaAppApp.swift
+//  AviaApp
+//
+//  Created by Антон Пеньков on 03.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct AviaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
